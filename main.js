965491bfd017193e64ee186ab0684ac27ed74dd9
Hossein <abdollahzadeh.hossein@gmail.com>
2023-11-07 09:46:18 +0330

    step 1 = basics

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-alert('Hello Workshop');
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import {Map, View} from 'ol';
+import {fromLonLat} from 'ol/proj';
+
+new Map({
+    target: 'map-container',
+    layers: [
+        new TileLayer({
+            source: new OSM(),
+        }),
+    ],
+    view: new View({
+        center: fromLonLat([0, 0]),
+        zoom: 2,
+    }),
+});
