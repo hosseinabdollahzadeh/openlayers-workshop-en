4bfa23bd8c675a8aa7a949ef9d623a05a979eea8
Hossein <abdollahzadeh.hossein@gmail.com>
2023-11-09 09:09:26 +0330

    Step 2 = Vector Data (Downloading features)

--- main.js
+++ main.js
@@ -61,3 +61,17 @@ map.addInteraction(
         source: source,
     })
 );
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', function () {
+    source.clear();
+});
+
+const format = new GeoJSON({featureProjection: 'EPSG:3857'});
+const download = document.getElementById('download');
+source.on('change', function () {
+    const features = source.getFeatures();
+    const json = format.writeFeatures(features);
+    download.href =
+        'data:application/json;charset=utf-8,' + encodeURIComponent(json);
+});
