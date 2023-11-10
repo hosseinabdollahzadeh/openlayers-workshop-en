0171957f092b6a72a31cd597933dc75e040354f0
Hossein <abdollahzadeh.hossein@gmail.com>
2023-11-10 11:36:21 +0330

    Step 3 = Mobile Maps and Sensors (Geolocation - Show the user's location)

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ import OSM from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
 import {Map, View} from 'ol';
 import {fromLonLat} from 'ol/proj';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import {circular} from 'ol/geom/Polygon';
+import Control from 'ol/control/Control';
 
-new Map({
+
+const map = new Map({
     target: 'map-container',
     layers: [
         new TileLayer({
@@ -15,3 +22,46 @@ new Map({
         zoom: 2,
     }),
 });
+
+const source = new VectorSource();
+const layer = new VectorLayer({
+    source: source,
+});
+map.addLayer(layer);
+
+navigator.geolocation.watchPosition(
+    function (pos) {
+        const coords = [pos.coords.longitude, pos.coords.latitude];
+        const accuracy = circular(coords, pos.coords.accuracy);
+        source.clear(true);
+        source.addFeatures([
+            new Feature(
+                accuracy.transform('EPSG:4326', map.getView().getProjection())
+            ),
+            new Feature(new Point(fromLonLat(coords))),
+        ]);
+    },
+    function (error) {
+        alert(`ERROR: ${error.message}`);
+    },
+    {
+        enableHighAccuracy: true,
+    }
+);
+
+const locate = document.createElement('div');
+locate.className = 'ol-control ol-unselectable locate';
+locate.innerHTML = '<button title="Locate me">◎</button>';
+locate.addEventListener('click', function () {
+    if (!source.isEmpty()) {
+        map.getView().fit(source.getExtent(), {
+            maxZoom: 18,
+            duration: 500,
+        });
+    }
+});
+map.addControl(
+    new Control({
+        element: locate,
+    })
+);
