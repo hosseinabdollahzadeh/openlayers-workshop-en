c91f5d43a778963cdee968653b912db8658ee865
Hossein <abdollahzadeh.hossein@gmail.com>
2023-11-11 19:03:16 +0330

    Step 4 = GeoTIFF Rendering (False color composite)

--- main.js
+++ main.js
@@ -16,10 +16,23 @@ const sourceExtent = [300000, 6090260, 409760, 6200020];
 const source = new GeoTIFF({
     sources: [
         {
-            url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A/TCI.tif',
+            // near-infrared reflectance
+            url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A/B08.tif',
+            max: 5000,
+        },
+        {
+            // red reflectance
+            url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A/B04.tif',
+            max: 5000,
+        },
+        {
+            // green reflectance
+            url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A/B03.tif',
+            max: 5000,
         },
     ],
 });
+
 
 const layer = new TileLayer({
     source: source,
